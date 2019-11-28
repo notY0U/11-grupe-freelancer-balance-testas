3b1ab987d30c87ae0f0dba077a4c163000bb6b1c
notY0U <57098864+notY0U@users.noreply.github.com>
2019-11-28 13:01:33 +0200

    account data order

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
 const account = [
-    { month: 0, income: 150 },
     { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
     { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
+    { month: 3, income: 450, expense: 200 },
     { month: 4, income: 10, expense: 300 },
+    { month: 5, income: 150, expense: 50 },
     { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
+    { month: 7, expense: 50 },
     { month: 8, income: 450, expense: 120 },
+    { month: 9, income: 700, expense: 400 },
+    { month: 10, income: 150 },
+    { month: 11, income: 150, expense: 30 },
     { month: 12, expense: 200 }
 ];
 
